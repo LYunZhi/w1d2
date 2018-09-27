9e5fa7605d8eca0bfe0f52fb13e1f26797ed33e3
Jim <yjzlin@gmail.com>
2018-09-27 18:14:25 +0000

    Modified to use a forEach loop instead of for loop

--- callbackintro.js
+++ callbackintro.js
@@ -1,10 +1,18 @@
 // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(item, i) {
+    if (item === 'Waldo') {
+      found(i)
     }
-  }
+  })
 }
 
 function actionWhenFound(i) {
